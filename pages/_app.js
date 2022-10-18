eda3fbfa44d28b66faa7272693d88ccecf09e075
ahn-sujin <asj9674@gmail.com>
2022-10-18 11:20:15 +0900

    Add:global css 활용하여 레이아웃 작업 완료

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import "../styles/globals.css";
 
 export default function MyApp({ Component, pageProps }) {
+  console.log("pageProps:", pageProps);
   return <Component {...pageProps} />;
 }
--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -17,9 +17,6 @@ const FirstPost = () => {
         }
       />
       <h1>First Post</h1>
-      <h2>
-        <Link href="/">Back to home</Link>
-      </h2>
     </Layout>
   );
 };
